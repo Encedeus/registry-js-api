e0f0420ed626408c9d9adb8a41d70d604b465685
OptimusePrime <karlo.vizec@skole.hr>
2024-02-07 08:51:41 +0100

    added protobuf compile script

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 rm -rf ./src/proto
 mkdir ./src/proto
-protoc --proto
+protoc --plugin=node_modules/ts-proto/protoc-gen-ts_proto --ts_proto_out=./src/proto --ts_proto_opt=esModuleInterop=true ../plugin-server/protobuf/* -I=../plugin-server/protobuf
